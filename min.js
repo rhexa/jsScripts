a532f152031bc4ed71226617d061c3ce5a65a9e9
rhexa <44338032+rhexa@users.noreply.github.com>
2021-02-05 16:55:28 +0200

    Update min.js

--- min.js
+++ min.js
@@ -1,12 +1,12 @@
 var a;
 
     function hideCollapse(elementId) {
-        if (a == 1){
-            document.getElementById(elementId).style.display="none";
-            a=0;
-        }
-        else {
+        if (a == 0){
             document.getElementById(elementId).style.display="inline";
             a=1;
+        }
+        else {
+            document.getElementById(elementId).style.display="none";
+            a=0;
         }
     }
