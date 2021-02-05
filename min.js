fc759e66581dfa19acef9867d607e966b139e74e
rhexa <44338032+rhexa@users.noreply.github.com>
2021-02-06 00:01:06 +0200

    Update min.js

--- min.js
+++ min.js
@@ -1,12 +1,50 @@
-var a;
 
-    function hideCollapse(elementId) {
-        if (a == 0){
-            document.getElementById(elementId).style.display="inline";
-            a=1;
-        }
-        else {
-            document.getElementById(elementId).style.display="none";
-            a=0;
-        }
-    }
+            var accordion = [];
+            var a;
+
+            function hideCollapse(elementId, collapse) {
+                if (accordion.length == 0) {
+                    console.log("added " + elementId);
+                    if (collapse == true) {
+                        document.getElementById(elementId).style.display="none";
+                        accordion.push([elementId, false]);
+                    }
+                    else {
+                        document.getElementById(elementId).style.display="inline";
+                        accordion.push([elementId, true]);
+                    }
+                }
+                    
+                else {
+                    var i;
+                    var found = false;
+                    for (i=0; i<accordion.length; i++){
+                        if (accordion[i][0] == elementId) {
+                            console.log("ID Found");
+                            found = true;
+                            if (accordion[i][1] == true){
+                                document.getElementById(elementId).style.display="none";
+                                accordion[i][1]=false;
+                                break;
+                            }
+                            else {
+                                document.getElementById(elementId).style.display="inline";
+                                accordion[i][1]=true;
+                                break;
+                            }
+                        }
+                    }
+                    if (!found){
+                        console.log("Not found " + elementId);
+                        if (collapse == true) {
+                            document.getElementById(elementId).style.display="none";
+                            accordion.push([elementId, false]);
+                        }
+                        else {
+                            document.getElementById(elementId).style.display="inline";
+                            accordion.push([elementId, true]);
+                        }
+                        found = false;
+                    }
+                }   
+            }
